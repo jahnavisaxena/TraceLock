21ebe3ec1f2888f49c5a37c3a89acd6986fffd7a
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-11-08 01:19:46 +0530

    Update config.go

--- config.go
+++ config.go
@@ -1,1 +1,32 @@
 
+package main 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	MonitorDir string `json:"monitor_dir"`
+	LogFile    string `json:"log_file"`
+}
+
+func LoadConfig() (Config, error) {
+	var cfg Config
+
+	file, err := os.Open("config.json")
+	if err != nil {
+		fmt.Println("  No config.json found — using defaults.")
+		cfg.MonitorDir = "./watched"
+		cfg.LogFile = "./logs/fim.log"
+		return cfg, nil
+	}
+	defer file.Close()
+
+	err = json.NewDecoder(file).Decode(&cfg)
+	if err != nil {
+		return cfg, err
+	}
+	return cfg, nil
+}
+
